a12c315ce26f60d959d269d6e9a440d2806120b1
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2022-09-12 20:23:24 -0400

    Perfect roatation
    fix rotation flip problem

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const DOMSelectors = {
+  card: document.getElementById("card"),
+  container: document.getElementById("container"),
+  next: document.getElementById("next"),
+};
+
+DOMSelectors.card.addEventListener("click", flipCard);
+
+function flipCard() {
+  DOMSelectors.container.classList.toggle("flipCard");
+}
